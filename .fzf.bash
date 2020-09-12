e0f0aad41d400833b8d344cb8bf3045dadf72716
Michael Rommel <rommel@layer-7.net>
2020-09-12 21:36:06 +0200

    Revert fzf's fd command line options

--- .fzf.bash
+++ .fzf.bash
@@ -15,6 +15,6 @@ fi
 [[ -d "${HOME}/.fzf/" ]] && source "${HOME}/.fzf/shell/key-bindings.bash"
 
 if [[ -x "/usr/bin/fd" ]]; then
-  export FZF_DEFAULT_COMMAND='fd --type file --hidden --exclude .git'
+  export FZF_DEFAULT_COMMAND='fd --type file'
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 fi
--- .fzf.zsh
+++ .fzf.zsh
@@ -15,7 +15,7 @@ fi
 [[ -d "${HOME}/.fzf/" ]] && source "${HOME}/.fzf/shell/key-bindings.zsh"
 
 if [[ -x "/usr/bin/fd" ]]; then
-  export FZF_DEFAULT_COMMAND='fd --type file --hidden --exclude .git'
+  export FZF_DEFAULT_COMMAND='fd --type file'
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 fi
 
