5788a59cf41056a9e2d9cb0eea8d4577f4028803
Michael Rommel <rommel@layer-7.net>
2021-01-08 13:17:03 +0100

    Add .gitignore link at installation time

--- bin/install_dotfiles.linux.sh
+++ bin/install_dotfiles.linux.sh
@@ -73,6 +73,7 @@ ln -sf .dotfiles/.fzf.zsh .
 ln -sf .dotfiles/.p10k.zsh .
 ln -sf .dotfiles/.less_colors.sh .
 ln -sf .dotfiles/.dir_colors.sh .
+ln -sf .dotfiles/.gitignore .
 ln -sf .dotfiles/.initialization.sh .
 ln -sf .dotfiles/.initialization.sh .bash_aliases
 touch .hushlogin
--- bin/install_dotfiles.macos.sh
+++ bin/install_dotfiles.macos.sh
@@ -25,6 +25,7 @@ ln -sf .dotfiles/.zshrc .
 ln -sf .dotfiles/.p10k.zsh .
 ln -sf .dotfiles/.less_colors.sh .
 ln -sf .dotfiles/.dir_colors.sh .
+ln -sf .dotfiles/.gitignore .
 ln -sf .dotfiles/.initialization.sh .
 ln -sf .dotfiles/.initialization.sh .bash_aliases
 touch .hushlogin
