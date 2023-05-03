c28b49550189886b60f14af570d5be4b617a867f
Michael Rommel <rommel@layer-7.net>
2023-05-03 22:44:46 +0200

    feat: add after dir to activate ft type plugins

---
+++ .config/miro/after/ftplugin/gitcommit.lua
@@ -0,0 +1,7 @@
+-- not to read twice
+if vim.g.cmp_gitcommit_source_id ~= nil then
+	require('cmp').unregister_source(vim.g.cmp_gitcommit_source_id)
+end
+
+vim.g.cmp_gitcommit_source_id = require('cmp').register_source('gitcommit',
+	require('cmp-gitcommit').new())
