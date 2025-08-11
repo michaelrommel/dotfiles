cb2ce2b3a3cdeb7d153b4d358cd9738e24b48ce3
Michael Rommel <rommel@layer-7.net>
2025-08-11 20:16:23 +0200

    fix: make minimal vim config functional with formatting

--- dot_config/minimal/init.lua
+++ dot_config/minimal/init.lua
@@ -1,21 +1,3 @@
-utf8 = function(decimal)
-	local bytemarkers = { { 0x7FF, 192 }, { 0xFFFF, 224 }, { 0x1FFFFF, 240 } }
-	if decimal < 128 then return string.char(decimal) end
-	local charbytes = {}
-	for bytes, vals in ipairs(bytemarkers) do
-		if decimal <= vals[1] then
-			for b = bytes + 1, 2, -1 do
-				local mod = decimal % 64
-				decimal = (decimal - mod) / 64
-				charbytes[b] = string.char(128 + mod)
-			end
-			charbytes[1] = string.char(vals[2] + decimal)
-			break
-		end
-	end
-	return table.concat(charbytes)
-end
-
 vim.cmd("syntax off")
 
 vim.opt.winborder = "rounded"
@@ -32,13 +14,13 @@ vim.opt.relativenumber = true
 vim.opt.termguicolors = true
 vim.opt.undofile = true
 vim.opt.signcolumn = "yes:1"
-vim.opt.listchars = { tab = utf8(0xBB) .. ' ', trail = utf8(0xB7), nbsp = '~' }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = '~' }
 vim.opt.list = true
 vim.opt.completeopt = "menuone,noinsert,noselect"
 vim.opt.linebreak = true
 vim.opt.breakindent = true
 vim.opt.autoindent = true
-vim.opt.showbreak = " " .. utf8(0xf17aa) .. " "
+vim.opt.showbreak = " 󱞪 "
 vim.opt.formatoptions = "jcro/qnp"
 
 -- treat zsh like bash
@@ -49,9 +31,7 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
 
 vim.g.mapleader = " "
 
--- remove the default mapping of Y to y$
 vim.keymap.del('n', 'Y')
-
 local map = vim.keymap.set
 map('n', '<leader>v', ':e $MYVIMRC<CR>')
 map('n', '<leader>o', ':update<CR> :source<CR>')
@@ -64,6 +44,7 @@ map({ 'n', 'v' }, '<leader>c', '1z=')
 vim.pack.add({
 	{ src = "https://github.com/projekt0n/github-nvim-theme" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/stevearc/conform.nvim" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 	{ src = 'https://github.com/neovim/nvim-lspconfig' },
 	{ src = "https://github.com/mason-org/mason.nvim" },
@@ -80,18 +61,36 @@ require("github-theme").setup({
 vim.cmd("colorscheme github_dark_high_contrast")
 vim.cmd(":hi statusline guibg=#666666 guifg=White")
 
-require "mason".setup()
-local oil = require "oil"
-oil.setup()
-map('n', '-', function() oil.open_float() end)
+require("mason").setup()
+
+require("oil").setup()
+map('n', '-', function() require("oil").open_float() end)
 
 require('nvim-treesitter').setup({ 
-	 -- Directory to install parsers and queries to
-	install_dir = vim.fn.stdpath('data') .. '/site',
 	highlight = { enable = true, },
 })
-require'nvim-treesitter'.install { 'lua', 'python' }
+require("nvim-treesitter").install { 'lua', 'python' }
 
-vim.lsp.enable({ "lua_ls", "jedi_language_server" })
+require("conform").setup({
+	formatters_by_ft = {
+		python = { "ruff_format" },
+	},
+		format_on_save = {
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
+})
+
+vim.lsp.enable({ "lua_ls", "jedi_language_server", "ruff" })
+require("lspconfig").lua_ls.setup({
+	settings = {
+		Lua = {
+			 diagnostics = {
+			  globals = { 'vim' },
+			 },
+		},
+	},
+})
 
 vim.diagnostic.config({ virtual_text = true })
+
