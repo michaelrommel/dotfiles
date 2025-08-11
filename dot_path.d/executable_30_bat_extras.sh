dd663b6638b19209423c65b03a9c15a543cfa018
Michael Rommel <rommel@layer-7.net>
2025-08-11 13:53:42 +0200

    fix: move bat-extras on linux

--- dot_path.d/executable_30_bat_extras.sh
+++ dot_path.d/executable_30_bat_extras.sh
@@ -3,20 +3,8 @@
 ARCH=$(uname -m)
 OS=$(uname -s)
 
-if [[ "${OS}" == "Darwin" ]]; then
-	if [[ "${ARCH}" == "arm64" ]]; then
-		# homebrew is installed in /opt/homebrew
-		WHERE=${HOME}/.bat
-	else
-		# homebrew is installed under /usr/local
-		WHERE=${HOME}/.bat
-	fi
-else
-	WHERE=${HOME}/.bat
-fi
-
 # Setup bat-extras
 # ----------------
-if [[ ! ":${PATH}:" == *:${WHERE}/bin:* ]]; then
-	export PATH="${WHERE}/bin${PATH:+:${PATH}}"
+if [[ ! ":${PATH}:" == *:${HOME}/software/bat/bin:* ]]; then
+	export PATH="${HOME}/software/bat/bin${PATH:+:${PATH}}"
 fi
