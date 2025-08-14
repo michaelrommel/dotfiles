18eae6a61281b9d92bddd15a75c302d70656f988
Michael Rommel <rommel@layer-7.net>
2025-08-14 20:50:11 +0200

    fix: optimize standard nvim startup

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,95 @@
+vim.cmd("syntax off")
+
+vim.opt.winborder = "rounded"
+vim.opt.hlsearch = true
+vim.opt.tabstop = 4
+vim.opt.cursorcolumn = false
+vim.opt.ignorecase = true
+vim.opt.expandtab = false
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.undofile = true
+vim.opt.signcolumn = "yes:1"
+-- vim.opt.listchars = { tab = "» ", trail = "·", nbsp = '~' }
+vim.opt.list = true
+vim.opt.completeopt = "menuone,noinsert,noselect"
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+vim.opt.autoindent = true
+-- vim.opt.showbreak = " 󱞪 "
+vim.opt.formatoptions = "jcro/qnp"
+
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = { "python" },
+	callback = function()
+		local ok, parser = pcall(vim.treesitter.get_parser, 0, "python")
+		if ok and parser ~= nil then
+			vim.treesitter.start()
+		end
+	end,
+})
+
+vim.keymap.del('n', 'Y')
+
+vim.pack.add({
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/stevearc/conform.nvim" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+	{ src = 'https://github.com/neovim/nvim-lspconfig' },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
+})
+
+vim.cmd(":hi statusline guibg=#666666 guifg=White")
+vim.cmd("colorscheme lunaperche")
+
+require("mason").setup()
+require("lspconfig").lua_ls.setup({
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { 'vim' },
+			},
+		},
+	},
+})
+require("mason-lspconfig").setup {
+	ensure_installed = { "lua_ls", "jedi_language_server", "ruff" },
+}
+require("oil").setup()
+vim.keymap.set('n', '-', function() require("oil").open_float() end)
+
+require("conform").setup({
+	formatters_by_ft = {
+		python = { "ruff_format" },
+	},
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
+})
+require('nvim-treesitter').setup({
+	highlight = { enable = true, },
+})
+require("nvim-treesitter").install { 'lua', 'python' }
+
+local servers = { "lua_ls", "jedi_language_server", "ruff" }
+local function is_installed(server_name)
+	local installed_servers = require("mason-lspconfig").get_installed_servers()
+	for _, name in ipairs(installed_servers) do
+		if name == server_name then
+			return true
+		end
+	end
+	return false
+end
+for _, server in ipairs(servers) do
+	if is_installed(server) then
+		vim.lsp.enable({ server })
+	end
+end
+vim.diagnostic.config({ virtual_text = true })
