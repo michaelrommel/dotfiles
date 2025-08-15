f5d9cfa1398cfba84e3b4b09d8230427f8ce27dd
Michael Rommel <rommel@layer-7.net>
2025-08-15 20:23:17 +0200

    fix: cmake compatibility for fzf-native

--- dot_config/miro/lua/plugins/telescope-fzf-native.lua
+++ dot_config/miro/lua/plugins/telescope-fzf-native.lua
@@ -3,5 +3,6 @@ return {
 	"nvim-telescope/telescope-fzf-native.nvim",
 	lazy = true,
 	build =
-	"cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+	"cmake -S. -Bbuild -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+	-- "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
 }
