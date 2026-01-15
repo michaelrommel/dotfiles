c2a6c8bbf08b3f7874eae6b879c93f9f34b0bbf3
Michael Rommel <rommel@layer-7.net>
2026-01-15 20:10:22 +0100

    fix: tool installations for fd, rg, bat

---
+++ bin/executable_install_bat.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+source "${HOME}/bin/helper.sh"
+
+if ! bat -V >/dev/null 2>&1; then
+	latest=$(curl -sL https://api.github.com/repos/sharkdp/bat/releases | jq -r ".[0].tag_name") || exit 1
+
+	if is_amzn; then
+		echo "Installing bat locally from github"
+		echo "Latest release seems to be: ${latest}"
+		cd "${HOME}" || exit
+		mkdir -p "${HOME}/software/archives"
+		cd "${HOME}/software/archives" || exit
+		curl -OL https://github.com/sharkdp/bat/releases/download/${latest}/bat-${latest}-${ARCH}-unknown-linux-gnu.tar.gz
+		cd "${HOME}/software" || exit
+		tar xf archives/bat-${latest}-${ARCH}-unknown-linux-gnu.tar.gz
+		cd "bat-${latest}-${ARCH}-unknown-linux-gnu" || exit
+		cp bat "${HOME}/bin/bat"
+		cp bat.1 "${HOME}/.local/share/man/man1/bat.1"
+		cp autocomplete/_bat "${HOME}/.local/share/zsh/completions/_bat"
+		chmod 755 "${HOME}/.local/share/zsh/completions/_bat"
+		cp autocomplete/bat.bash "${HOME}/.local/share/bash-completion/completions/bat"
+	else
+		echo "Installing bat deb package from github"
+		echo "Latest release seems to be: ${latest}"
+		cd "${HOME}" || exit
+		mkdir -p "${HOME}/software/archives"
+		cd "${HOME}/software/archives" || exit
+		arch="$(get_arch)"
+		if [ "$arch" = x86_64 ]; then
+			arch="amd64"
+		elif [ "$arch" = aarch64 ]; then
+			arch="arm64"
+		else
+			echo "unsupported architecture: $arch"
+			exit 1
+		fi
+		curl -OL "https://github.com/sharkdp/bat/releases/download/${latest}/bat_${latest}_${arch}.deb"
+		sudo dpkg -i "${HOME}/software/archives/bat_${latest}_${arch}.deb"
+	fi
+fi
---
+++ bin/executable_install_ripgrep.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+source "${HOME}/bin/helper.sh"
+
+if ! fd -V >/dev/null 2>&1; then
+	latest=$(curl -sL https://api.github.com/repos/BurntSushi/ripgrep/releases | jq -r ".[0].tag_name") || exit 1
+
+	if is_amzn; then
+		echo "Installing ripgrep locally from github"
+		echo "Latest release seems to be: ${latest}"
+		# provides faster grep
+		cd "${HOME}" || exit
+		mkdir -p "${HOME}/software/archives"
+		cd "${HOME}/software/archives" || exit
+		arch="$(get_arch)"
+		if [ "$arch" = x86_64 ]; then
+			libc="musl"
+		elif [ "$arch" = aarch64 ]; then
+			libc="gnu"
+		else
+			echo "unsupported architecture: $arch"
+			exit 1
+		fi
+		curl -OL https://github.com/BurntSushi/ripgrep/releases/download/${latest}/ripgrep-${latest}-${ARCH}-unknown-linux-${libc}.tar.gz
+		cd "${HOME}/software" || exit
+		tar xf archives/ripgrep-${latest}-${ARCH}-unknown-linux-${libc}.tar.gz
+		cd "ripgrep-${latest}-${ARCH}-unknown-linux-${libc}" || exit
+		cp rg "${HOME}/bin/rg"
+		cp doc/rg.1 "${HOME}/.local/share/man/man1/rg.1"
+		cp complete/_rg "${HOME}/.local/share/zsh/completions/_rg"
+		chmod 755 "${HOME}/.local/share/zsh/completions/_rg"
+		cp complete/rg.bash "${HOME}/.local/share/bash-completion/completions/rg"
+	else
+		echo "Installing ripgrep deb package from github"
+		echo "Latest release seems to be: ${latest}"
+		# provides faster grep
+		cd "${HOME}" || exit
+		mkdir -p "${HOME}/software/archives"
+		cd "${HOME}/software/archives" || exit
+		arch="$(get_arch)"
+		if [ "$arch" = x86_64 ]; then
+			arch="amd64"
+		elif [ "$arch" = aarch64 ]; then
+			arch="arm64"
+		else
+			echo "unsupported architecture: $arch"
+			exit 1
+		fi
+		curl -OL "https://github.com/BurntSushi/ripgrep/releases/download/${latest}/ripgrep_${latest}_${arch}.deb"
+		sudo dpkg -i "${HOME}/software/archives/ripgrep_${latest}_${arch}.deb"
+	fi
+fi
